c34a960543dcfe763c8d47ddc673133169387c84
neven.shalata <nevenh.1994@gmail.com>
2022-08-01 09:56:19 +0300

    fetch:api-movies-homework

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 
 import './App.css';
-
+import Movies from './components/homework movies/movies/movies';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      
+      <Movies/>
       </header>
     </div>
   );
